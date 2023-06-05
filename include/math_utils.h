ae240fe5104e69619fc8b338059a364c8cba2292
datwaft <datwaftx@gmail.com>
2023-06-04 18:19:05 -0600

    feat: add LCM for arrays

--- include/math_utils.h
+++ include/math_utils.h
@@ -1,8 +1,11 @@
 #ifndef MATH_UTILS_H
 #define MATH_UTILS_H
 
+#include <stddef.h>
 #include <stdint.h>
 
 int64_t least_common_multiple(int64_t a, int64_t b);
+
+int64_t least_common_multiple_for_array(int64_t array[], size_t array_size);
 
 #endif /* !MATH_UTILS_H */
--- src/math_utils.c
+++ src/math_utils.c
@@ -11,3 +11,14 @@ int64_t least_common_multiple(int64_t a, int64_t b) {
   }
   return i;
 }
+
+int64_t least_common_multiple_for_array(int64_t array[], size_t array_size) {
+  if (array_size < 2) {
+    return -1;
+  } else if (array_size == 2) {
+    return least_common_multiple(array[0], array[1]);
+  } else {
+    return least_common_multiple(
+        array[0], least_common_multiple_for_array(array + 1, array_size - 1));
+  }
+}
